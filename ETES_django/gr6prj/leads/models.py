ed7b8f8c8e75a4c1d2c0f6530550334593d5cacb
John Tran <jooohhn@users.noreply.github.com>
2018-10-02 09:07:20 -0700

    Directory restructure (#1)
    * Renamed 'gr6prj' to 'project'
    
    * Removed unneeded tutorial files

--- ETES_django/gr6prj/leads/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Lead(models.Model):
-    firstName = models.CharField(max_length=100)
-    userName = models.CharField(max_length=50)
-    email = models.EmailField()
-    message = models.CharField(max_length=300)
-    created_at = models.DateTimeField(auto_now_add=True)
