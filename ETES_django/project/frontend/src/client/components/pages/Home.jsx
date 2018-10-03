d63b77ab394abc17a3c9841536642e4ce5edc1ee
John Tran <jptran318@gmail.com>
2018-10-02 22:13:13 -0700

    Added boilerplate React files

---
+++ ETES_django/project/frontend/src/client/components/pages/Home.jsx
@@ -0,0 +1,19 @@
+// @flow
+
+import React from 'react';
+import Helmet from 'react-helmet';
+import { APP_NAME } from '../../../config';
+
+const HomePage = () => (
+  <div>
+    <Helmet
+      meta={[
+        { name: 'description', content: 'At HomePage of Hello App' },
+        { property: 'og:title', content: APP_NAME }
+      ]}
+    />
+    <h1>At Home Page</h1>
+  </div>
+);
+
+export default HomePage;
