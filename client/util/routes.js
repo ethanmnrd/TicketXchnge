0801df9423c652b9bc56009fe186d4f748ebbbb3
John Tran <jptran318@gmail.com>
2018-11-18 20:33:04 -0800

    Added / to urls

--- client/util/routes.js
+++ client/util/routes.js
@@ -1,10 +1,11 @@
 // @flow
 
 export const HOME_PAGE_ROUTE = '/';
-export const LOGIN_PAGE_ROUTE = '/login';
-export const CHECKOUT_PAGE_ROUTE = '/checkout';
-export const SELL_PAGE = '/event/sell';
-export const CREATE_EVENT_PAGE = '/event/create';
-export const USERS_API_ROUTE = '/api/users';
-export const TICKETS_API_ROUTE = '/api/tickets';
-export const EVENTS_API_ROUTE = '/api/events';
+export const LOGIN_PAGE_ROUTE = '/login/';
+export const CHECKOUT_PAGE_ROUTE = '/checkout/';
+export const SELL_PAGE = '/event/sell/';
+export const CREATE_EVENT_PAGE = '/event/create/';
+export const USERS_API_ROUTE = '/api/users/';
+export const LOGIN_AUTH_API_ROUTE = '/api/login/';
+export const TICKETS_API_ROUTE = '/api/tickets/';
+export const EVENTS_API_ROUTE = '/api/events/';
--- server/etes/urls.py
+++ server/etes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
-    path('users', views.UserCreate.as_view(), name="users-all"),
-    path('tickets', views.TicketListCreate.as_view(), name="tickets-all"),
+    path('users/', views.UserCreate.as_view(), name="users-all"),
+    path('tickets/', views.TicketListCreate.as_view(), name="tickets-all"),
     path('ticket/<tid>', views.TicketRetrieveDestroy.as_view()),
-    path('events', views.EventCreate.as_view(), name="events-all"),
-    path('login',  views.UserAuth.as_view()),
+    path('events/', views.EventCreate.as_view(), name="events-all"),
+    path('login/',  views.UserAuth.as_view()),
 ]
