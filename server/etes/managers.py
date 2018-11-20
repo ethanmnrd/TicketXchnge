6f32d0ccb35a85bfce4d9d57685895b218939fe5
John Tran <jptran318@gmail.com>
2018-11-19 22:42:21 -0800

    Fixed tests

--- server/etes/managers.py
+++ server/etes/managers.py
@@ -17,11 +17,9 @@ class UserManager(BaseUserManager):
 class TicketManager(models.Manager):
     use_in_migrations = True
 
-    def create_ticket(self, ticket_event, ticket_type, ticket_price, eventId, ownerId):
-        if not ticket_event:
-            raise ValueError('Must have event ticket is associated to')
+    def create_ticket(self, ticket_event, ticket_type, ticket_price, eventId, ticket_quantity, ownerId):
         ticket = self.model(ticket_event=ticket_event,
-                            ticket_type=ticket_type, ticket_price=ticket_price, owner=ownerId, event=eventId)
+                            ticket_type=ticket_type, ticket_price=ticket_price, ticket_quantity=ticket_quantity, owner=ownerId, event=eventId)
         ticket.save(using=self._db)
         return ticket
 
